ec781fdd6f150bffe7285a6b952e7f81d4aba277
achira-sketch <asdstdf@gmail.com>
2022-12-27 00:18:41 +0200

    NotFound page fix

--- pages/NotFound.js
+++ pages/NotFound.js
@@ -1,13 +1,10 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
 export default function NotFound() {
   return (
     <div>
       <h1>Oops! You seem to be lost.</h1>
       <p>Here are some helpful links:</p>
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/contact">Contact</Link>
     </div>
   );
 }
