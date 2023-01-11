8c85ffea5ed72ea90694add26cbce88aac37400a
achira-sketch <asdstdf@gmail.com>
2023-01-11 19:15:07 +0200

    webhooks attempt 3

--- pages/api/ttn-webhook.js
+++ pages/api/ttn-webhook.js
@@ -1,13 +1,8 @@
-export default async (req,res) => {
-  // handle the TTN webhook here
-  // you can access the webhook data in the `req.body` object
-  console.log(req.body);
-
-  // const body2 = (await buffer(req)).toString();
-  // const data = JSON.parse(body2);
-
-  // console.log(data);
-
-  // send a response to TTN to acknowledge receipt of the webhook
-  res.status(200).send('Webhook received');
+export default async (req, res) => {
+  if (req.method === 'POST') {
+      console.log(req.body);
+      res.status(200).json({ success: true });
+  } else {
+      res.status(405).end();
+  }
 }
