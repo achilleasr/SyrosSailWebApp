844552010e03b51d8f8105e16295cce2d75f7e70
achira-sketch <asdstdf@gmail.com>
2023-01-12 21:13:32 +0200

    fix store redux path

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 import { Provider } from 'react-redux';
 import { App } from 'next/app';
-import store from './store';
+// import store from 'store';
+import { createStore } from 'redux';
+
+const initialState = {
+    requestBody: {}
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'SET_REQUEST_BODY':
+            return { ...state, requestBody: action.payload };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+
+
 
 function MyApp({ Component, pageProps }) {
     return (
